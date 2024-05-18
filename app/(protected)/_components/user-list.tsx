4c0cddd419a34bea388c7c467e166dd3d49c3831
modernwallpaper <j.cunz@grimmels.de>
2024-05-18 20:13:29 +0200

    Added user table -> admin

--- app/(protected)/_components/user-list.tsx
+++ app/(protected)/_components/user-list.tsx
@@ -5,7 +5,11 @@ import { DataTable } from "./user-table/data-table"
 import { User, columns } from "./user-table/columns"
 
 export const UserList = async () => {
-  const users: User[] = await getAllUsers()
+  const users = await getAllUsers()
+
+  if(!users) return <div>No users found.</div>
+
+  if('error' in users) return <div>{users.error}</div>
 
   return(
     <DataTable columns={columns} data={users}></DataTable>
