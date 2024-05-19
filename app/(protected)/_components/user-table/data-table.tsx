d5daec36c1fe5f22692b5ddecec57cb097d02698
modernwallpaper <j.cunz@grimmels.de>
2024-05-19 14:11:13 +0200

    Changed table layout -> admin

--- app/(protected)/_components/user-table/data-table.tsx
+++ app/(protected)/_components/user-table/data-table.tsx
@@ -77,7 +77,7 @@ export function DataTable<TData, TValue>({
           onChange={(event) =>
             table.getColumn("name")?.setFilterValue(event.target.value)
           }
-          className="max-w-sm"
+          className="md:max-w-sm w-60"
         />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
